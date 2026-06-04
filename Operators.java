2de7667d2b62931a7f5b041799bdfc6d35911af6
shubham0323 <123639780+shubham0323@users.noreply.github.com>
2026-06-04 08:26:20 +0530

    Add various Java examples demonstrating conditionals, operators, switch statements, ternary operations, data types, type conversion, and variable usage

---
+++ Operators.java
@@ -0,0 +1,110 @@
+
+public class Operators{
+
+	public static void main(String[] args) {
+/**		int num1=7;
+		int num2=5;
+		int result=num1+num2;
+		System.out.println(result);
+**/	
+		
+/**
+		int num1=7;
+		int num2=5;
+		int result=num1-num2;
+		System.out.println(result);
+**/
+		
+/**		
+		int num1=7;
+		int num2=5;
+		int result=num1*num2;
+		System.out.println(result);
+**/
+		
+/**
+		int num1=7;
+		int num2=5;
+		int result=num1/num2;
+		System.out.println(result);
+**/
+		
+/**
+		int num1=7;
+		int num2=5;
+		int result=num1%num2;
+		System.out.println(result);
+**/
+		
+		int num=7;
+//		num=num+2;
+//		num+=2;
+//		num*=2;
+		
+		num++;    //post increment
+		++num;    //pre increment
+		num--;    //post decrement
+		--num;	  //pre decrement
+		System.out.println(num);
+		
+		int result=num++; //fetch the value and then increment
+		System.out.println(result);		
+	
+	}
+
+}
+
+
+
+// Relational Operator
+/*public class Operators {
+
+	public static void main(String[] args) {
+		int x=6;
+		int y=5;
+//		boolean result= x<y;
+//		boolean result= x>y;
+//		boolean result= x>=y;
+//		boolean result= x<=y;
+//		boolean result= x!=y;
+		boolean result= x==y;
+		System.out.println(result);
+		
+		double a=8.8;
+		double b=9.8;
+//		boolean res = a<=b;		
+		boolean res = a>=b;
+	
+		System.out.println(res);
+	}
+
+}
+    */
+
+// Logical Operator
+
+/*public class Operators {
+
+	public static void main(String[] args) {
+		int x=7;
+		int y=5;
+		int a=5;
+		int b=9;
+		
+//		boolean result= x>y && a<b ;
+//		boolean result= x>y || a<b ;
+//		boolean result= x>y && a>b ;
+//		boolean result= x>y || a>b ;
+//		boolean result= x<y && a<b ;
+//		boolean result= x<y || a<b ;
+//		boolean result= x<y || a<b || a>1 ;
+		
+//		System.out.println(result);
+		
+		boolean result= a>b ;
+		System.out.println(!result);
+		
+	}
+
+}
+*/
---
+++ variables.java
@@ -0,0 +1,17 @@
+class variables
+{
+    public static void main( String a [] )
+    {
+        int num1 = 10;
+        double num2 = 12.12;
+        float num3 = 2.22f;
+
+        double result = num1 + num2 + num3;
+
+        System.out.println( result);
+    }
+}
+
+
+
+
